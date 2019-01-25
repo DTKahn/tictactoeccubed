36f278b592fe35ebda7bb80eb38734883a274ed0
Daniel <dtkahn@gmail.com>
2019-01-24 19:08:08 -0500

    handleBoxClick method for changing box from null to player

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -8,58 +8,80 @@ class GameArea extends Component {
     super();
     this.state = {
       playerTurn : "player1",
-      // gameBoards : [
-      //   [
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null]
-      //   ],
-      //   [
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null]
-      //   ],
-      //   [
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null]
-      //   ],
-      //   [
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null],
-      //     [null, null, null, null]
-      //   ]
       gameBoards : [
         [
-          ["P1", null, null, null],
-          [null, "P1", null, null],
-          [null, null, "P2", null],
-          [null, null, null, "P1"]
-        ],
-        [
           [null, null, null, null],
-          ["P2", null, null, null],
           [null, null, null, null],
-          [null, null, null, "P2"]
-        ],
-        [
-          [null, null, "P2", null],
-          [null, null, "P2", null],
-          ["P1", null, null, null],
-          [null, null, "P2", null]
+          [null, null, null, null],
+          [null, null, null, null]
         ],
         [
           [null, null, null, null],
           [null, null, null, null],
           [null, null, null, null],
-          ["P1", null, null, "P1"]
+          [null, null, null, null]
+        ],
+        [
+          [null, null, null, null],
+          [null, null, null, null],
+          [null, null, null, null],
+          [null, null, null, null]
+        ],
+        [
+          [null, null, null, null],
+          [null, null, null, null],
+          [null, null, null, null],
+          [null, null, null, null]
         ]
+      // gameBoards : [
+      //   [
+      //     ["P1", null, null, null],
+      //     [null, "P1", null, null],
+      //     [null, null, "P2", null],
+      //     [null, null, null, "P1"]
+      //   ],
+      //   [
+      //     [null, null, null, null],
+      //     ["P2", null, null, null],
+      //     [null, null, null, null],
+      //     [null, null, null, "P2"]
+      //   ],
+      //   [
+      //     [null, null, "P2", null],
+      //     [null, null, "P2", null],
+      //     ["P1", null, null, null],
+      //     [null, null, "P2", null]
+      //   ],
+      //   [
+      //     [null, null, null, null],
+      //     [null, null, null, null],
+      //     [null, null, null, null],
+      //     ["P1", null, null, "P1"]
+      //   ]
       ]
     }
+  }
+
+  handleBoxClick = (board, col, row, player) => {
+    let gameBoardsCopy = this.state.gameBoards
+
+    gameBoardsCopy[board][col][row] = player
+
+    this.setState({gameBoards : gameBoardsCopy})
+  }
+
+  componentDidMount(){
+    // Test
+    this.handleBoxClick(0, 0, 2, "P1")
+    this.handleBoxClick(0, 0, 3, "P2")
+    this.handleBoxClick(1, 2, 2, "P1")
+    this.handleBoxClick(0, 0, 0, "P2")
+    this.handleBoxClick(3, 2, 1, "P1")
+    this.handleBoxClick(2, 2, 0, "P2")
+    this.handleBoxClick(0, 3, 2, "P1")
+    this.handleBoxClick(1, 0, 1, "P2")
+    this.handleBoxClick(0, 1, 3, "P1")
+    this.handleBoxClick(2, 1, 3, "P2")
   }
 
   render() {
